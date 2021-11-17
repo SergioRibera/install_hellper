49222373ac555a34552db223965deb0b0e23806c
SergioRibera <sergioalejandroriberacosta@gmail.com>
2021-11-17 02:58:15 -0400

    Modify structure

--- src/structs.rs
+++ src/structs.rs
@@ -15,13 +15,39 @@ fn parse_line (l: String) -> Option<(String, String)> {
 
     return Some((String::from(words[0]), trim_value))
 }
+pub fn get_os() -> Result<OsTarget, Error> {
+    match env::consts::OS {
+        "windows" => Ok(OsTarget::Windows),
+        "macos" => Ok(OsTarget::Mac),
+        "linux" => {
+            let mut s = String::new();
+            File::open("/etc/os-release")?.read_to_string(&mut s)?;
+
+            let mut info: (String, String) = ("linux".to_string(), "Linux".to_string());
+            for l in s.split('\n') {
+                match parse_line (l.trim().to_string()) {
+                    Some((key, value)) =>
+                        match (key.as_ref(), value) {
+                            ("ID", val) => info.0 = val,
+                            ("PRETTY_NAME", val) => info.1 = val,
+                            _ => {}
+                        }
+                    None => {}
+                }
+            }
+            Ok(OsTarget::Linux(info))
+        },
+        _ => Err(Error::UnsupportedSystem)
+    }
+}
 
 use std::env;
 use std::fs::File;
 use std::io::Read;
-use std::process::{Command, ExitStatus, Output};
+use std::process::Command;
 
-use eval::{Expr, to_value};
+use evalexpr::{ context_map, eval_boolean_with_context };
+use regex::Regex;
 use serde::Deserialize;
 
 use crate::err::Error;
@@ -62,6 +88,10 @@ pub struct Step {
     pub commands: Vec<Cmd>,
     #[serde(default = "default_os_target")]
     pub os_target: String, // Posibilities: Any, Windows, Mac, Linux
+    #[serde(default)]
+    pub show_outputs: bool,
+    #[serde(default)]
+    pub sentence: String,
 }
 #[derive(Deserialize, Debug, Clone, Default)]
 pub struct ConfigInstall {
@@ -72,34 +102,6 @@ pub struct Configs {
     pub config_install: ConfigInstall,
 }
 
-impl Configs {
-    pub fn get_os(self) -> Result<OsTarget, Error> {
-        match env::consts::OS {
-            "windows" => Ok(OsTarget::Windows),
-            "macos" => Ok(OsTarget::Mac),
-            "linux" => {
-                let mut s = String::new();
-                File::open("/etc/os-release")?.read_to_string(&mut s)?;
-
-                let mut info: (String, String) = ("linux".to_string(), "Linux".to_string());
-                for l in s.split('\n') {
-                    match parse_line (l.trim().to_string()) {
-                        Some((key, value)) =>
-                            match (key.as_ref(), value) {
-                                ("ID", val) => info.0 = val,
-                                ("PRETTY_NAME", val) => info.1 = val,
-                                _ => {}
-                            }
-                        None => {}
-                    }
-                }
-                Ok(OsTarget::Linux(info))
-            },
-            _ => Err(Error::UnsupportedSystem)
-        }
-    }
-}
-
 impl Default for Step {
     fn default() -> Self {
         Step {
@@ -108,22 +110,48 @@ impl Default for Step {
             type_step: String::from("Custom"),
             commands: Vec::new(),
             os_target: String::from("Any"),
+            show_outputs: false,
+            sentence: String::new(),
         }
     }
 }
 impl Step {
     #[allow(dead_code)]
-    pub fn exec(&mut self) {
+    pub fn exec(&mut self, mut options_selecteds: Vec<String>) {
         // TODO: only os compatible
-        let result_eval = Expr::new(self.sentence)
-               .value("foo", true)
-               .value("bar", true)
-               .exec()
+        let linux_distro: String = match get_os() {
+            Ok(OsTarget::Linux((name, _))) => name,
+            Ok(_) => "".to_string(),
+            Err(_) => "".to_string(),
+        };
+        let context = context_map! {
+            "os" => env::consts::OS,
+            "linux_distro" => linux_distro,
+        }.unwrap(); // Do proper error handling here
+        let sentence_eval: bool = eval_boolean_with_context(&self.sentence, &context)
                .unwrap();
-        if result_eval == to_value(true) {
+
+        // TODO: move regex to Installer and create new Step class for add more parametters
+        let exp_braces = Regex::new(r"\{\{|\}\}|\{([^}]+)\}").unwrap();
+
+        if sentence_eval == true || self.sentence.is_empty() {
             for cmd in &self.commands {
+                let mut new_args: Vec<String> = Vec::new();
+
+                for arg in cmd.args.clone() {
+                    for capture in exp_braces.captures_iter(arg.as_str()) {
+                        if let Some(matched) = capture.get(1) {
+                            if matched.as_str() == "options" {
+                                new_args.append(&mut options_selecteds);
+                            }
+                        }
+                    }
+                }
+                if new_args.len() == 0 {
+                    new_args = options_selecteds.clone();
+                }
                 let output = Command::new(cmd.cmd.clone())
-                    .args(cmd.args.clone())
+                    .args(new_args)
                     .output()
                     .expect("failed to execute process");
                 if self.show_outputs {
--- src/test.rs
+++ src/test.rs
@@ -1,34 +1,40 @@
 #[cfg(test)]
 mod tests {
-    use crate::structs::{ConfigInstall, Configs, Step};
+    use std::env;
+
+    use evalexpr::{context_map, eval_boolean_with_context};
+
+    use crate::structs::{Cmd, ConfigInstall, Configs, Step};
     use crate::{EasyCommandBuilder, load_configs_from_str};
 
     #[test]
     fn test_load_configs() {
-        let json_example = "
-            {
-                \"config_install\": {
-                    \"steps\": [
-                        {
-                            \"name\": \"Install Packages\",
-                            \"description\": \"Lorem\",
-                            \"type_step\": \"Custom\",
-                            \"cmd\": \"sudo pacman -S\",
-                            \"os_target\": \"linux\",
-                            \"args\": [
-                                \"alacritty\",
-                                \"zsh\"
-                            ]
-                        }
-                    ]
-                }
+        let json_example = r#"{
+            "config_install": {
+                "steps": [
+                    {
+                        "name": "Cloning Repo",
+                        "description": "Lorem",
+                        "commands": [
+                            {
+                                "cmd": "git",
+                                "args": [
+                                    "clone",
+                                    "https://alskfksdfh.alkjd",
+                                    "{options}"
+                                ]
+                            }
+                        ]
+                    }
+                ]
             }
-            ";
+        }"#;
         let config = load_configs_from_str(json_example);
         assert_eq!(config.config_install.steps.len(), 1);
         assert_eq!(config.config_install.steps[0].type_step, "Custom");
         assert_eq!(config.config_install.steps[0].description, "Lorem");
-        assert_eq!(config.config_install.steps[0].args.len(), 2);
+        assert_eq!(config.config_install.steps[0].commands.len(), 1);
+        assert_eq!(config.config_install.steps[0].os_target, "Any");
     }
 
     #[test]
@@ -38,10 +44,14 @@ mod tests {
                 steps: vec![ Step {
                     name: "Hello World".to_string(),
                     description: "The global".to_string(),
-                    type_step: "Custom".to_string(),
-                    cmd: "echo".to_string(),
-                    args: vec![ "\"Hello World!!\"".to_string() ],
+                    commands: vec![
+                        Cmd {
+                            cmd: "echo".to_string(),
+                            args: vec![ "\"Hello World!!\"".to_string() ],
+                        }
+                    ],
                     os_target: "Linux".to_string(),
+                    ..Default::default()
                 } ]
             }
         };
@@ -50,6 +60,34 @@ mod tests {
                         .build();
         assert_eq!(easy.current_step, 0);
         assert_eq!(easy.configs.config_install.steps.len(), 1);
-        assert_eq!(easy.configs.config_install.steps[0].cmd, "echo".to_string());
+        assert_eq!(easy.configs.config_install.steps[0].commands[0].cmd, "echo".to_string());
+        assert_eq!(easy.configs.config_install.steps[0].os_target, "Linux".to_string());
+    }
+
+    #[test]
+    fn test_sentence() {
+        let json_example = format!(r#"{{
+                "config_install": {{
+                    "steps": [
+                        {{
+                            "name": "Test sentence",
+                            "sentence": "os == \"{}\"",
+                            "commands": [ ]
+                        }}
+                    ]
+                }}
+            }}
+        "#, env::consts::OS);
+        let config = load_configs_from_str(json_example.as_str());
+
+        let context = context_map! {
+            "os" => env::consts::OS,
+        }.unwrap(); // Do proper error handling here
+
+        // println!("{}", config.config_install.steps[0].sentence);
+        assert_eq!(config.config_install.steps[0].sentence, format!("os == \"{}\"", env::consts::OS));
+        assert_eq!(
+            eval_boolean_with_context(&config.config_install.steps[0].sentence, &context),
+            Ok(true));
     }
 }
